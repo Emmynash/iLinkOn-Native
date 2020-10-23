eb994537162ec30555fae28f3273debbc04dcdfe
Akita Emmanuel <e.akita91@gmail.com>
2020-10-23 12:55:21 +0100

    set slidertxt fontsize for ios

--- src/screens/BoardingScreen/styles.js
+++ src/screens/BoardingScreen/styles.js
@@ -11,7 +11,7 @@ let styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    alignContent: 'center'
+    alignContent: 'center',
     // paddingTop: (Platform.OS) === 'ios' ? 20 : 0,
     // padding : 20
   },
@@ -21,20 +21,21 @@ let styles = StyleSheet.create({
     paddingBottom: 10,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'transparent'
+    backgroundColor: 'transparent',
   },
 
   sliderText: {
-    fontSize: defaultTheme.SmallFont,
+    fontSize:
+      Platform.OS === 'ios' ? defaultTheme.SmallerFont : defaultTheme.SmallFont,
     color: colors.darkSilver,
     backgroundColor: 'transparent',
     textAlign: 'center',
     fontFamily: defaultTheme.LightFont,
     width: '100%',
-    paddingHorizontal: 26
+    paddingHorizontal: 26,
   },
   sliderTitle: {
-    fontSize: 28,
+    fontSize: Platform.OS === 'ios' ? 22 : 28,
     marginBottom: 8,
     color: colors.itemColor,
     backgroundColor: 'transparent',
@@ -42,29 +43,29 @@ let styles = StyleSheet.create({
     marginTop: 50,
     fontFamily: defaultTheme.headerFont,
     width: '100%',
-    paddingHorizontal: 16
+    paddingHorizontal: 16,
   },
   image: {
     width: '90%',
     height: '40%',
     resizeMode: 'contain',
     alignSelf: 'center',
-    marginTop: '20%'
+    marginTop: '20%',
   },
 
   sliderDots: {
     backgroundColor: defaultTheme.secondaryTextColor,
-    marginBottom: 90
+    marginBottom: 90,
   },
   textView: {
     width: '100%',
-    padding: 20
+    padding: 20,
   },
   activeDotStyle: {
     backgroundColor: colors.gold,
     borderWidth: 1,
     borderColor: colors.gold,
-    marginBottom: 90
+    marginBottom: 90,
   },
 
   nextBtn: {
@@ -73,11 +74,11 @@ let styles = StyleSheet.create({
     backgroundColor: 'rgba(0, 0, 0, 0.9)',
     borderRadius: 8,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   backgroundImage: {
     width: '100%',
-    height: '100%'
+    height: '100%',
   },
   doneBtn: {
     width: 324,
@@ -86,7 +87,7 @@ let styles = StyleSheet.create({
     borderRadius: 8,
     justifyContent: 'center',
     alignItems: 'center',
-    marginRight: 10
+    marginRight: 10,
   },
   skipBtn: {
     width: 150,
@@ -96,18 +97,18 @@ let styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     borderWidth: 1.5,
-    borderColor: colors.gold
+    borderColor: colors.gold,
   },
   skipBtnText: {
     fontFamily: defaultTheme.headerFont,
     color: colors.itemColor,
-    fontSize: defaultTheme.SmallFont
+    fontSize: defaultTheme.SmallFont,
   },
   nextBtnText: {
     fontFamily: defaultTheme.headerFont,
     color: defaultTheme.whiteShade,
-    fontSize: defaultTheme.SmallFont
-  }
+    fontSize: defaultTheme.SmallFont,
+  },
 });
 
 export default styles;
