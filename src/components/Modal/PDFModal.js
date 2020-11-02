519789b61de865179e2fc340ee6914d413db1141
Akita Emmanuel <e.akita91@gmail.com>
2020-11-02 11:05:20 +0100

    Add pdf dowload, handle error and  success message

--- src/components/Modal/PDFModal.js
+++ src/components/Modal/PDFModal.js
@@ -7,25 +7,78 @@ import {
   Modal,
   TouchableWithoutFeedback,
   Dimensions,
+  TouchableOpacity,
+  Image,
+  StatusBar,
 } from 'react-native';
 import colors from '../../assets/colors';
-import PropTypes from 'prop-types';
 import theme from '../../assets/theme';
+import PropTypes from 'prop-types';
 import PDFReader from 'rn-pdf-reader-js';
+import * as FileSystem from 'expo-file-system';
+import * as Permissions from 'expo-permissions';
+import * as MediaLibrary from 'expo-media-library';
+import { ErrorAlert, SuccessAlert } from '../../components';
 
-export default class PDFModal extends Component {
+class PDFModal extends Component {
   constructor(props) {
     super(props);
     this.state = {
       modalVisible: false,
+      showAlert: false,
+      showSuccessAlert: false,
+      message: '',
     };
+    this.closeNotification = this.closeNotification.bind(this);
+    this._downloadPdf = this._downloadPdf.bind(this);
   }
 
   closeNotification = () => {
     return this.props.handleCloseModal();
   };
+
+  handleCloseNotification = () => {
+    return this.setState({
+      showAlert: false,
+      showSuccessAlert: false,
+    });
+  };
+
+  _downloadPdf = async () => {
+    const uri = this.props.uri;
+    let fileUri = FileSystem.documentDirectory + uri.split('/')[7];
+    FileSystem.downloadAsync(uri, fileUri)
+      .then(({ uri }) => {
+        if (uri === '' || undefined || null) {
+          return this.setState({
+            showAlert: true,
+            showSuccessAlert: false,
+            message: 'File could not be saved, check yourr network!',
+          });
+        }
+        this.saveFile(uri);
+        console.log(uri);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  saveFile = async (fileUri) => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    if (status === 'granted') {
+      const asset = await MediaLibrary.createAssetAsync(fileUri);
+      await MediaLibrary.createAlbumAsync('Download', asset, false);
+      return this.setState({
+        showAlert: true,
+        showSuccessAlert: false,
+        message: 'File successfully saved!',
+      });
+    }
+  };
   render() {
     const { visible, modalStyle } = this.props;
+    const { message, showAlert } = this.state;
     const style = modalStyle || styles.modalCont;
     return (
       <TouchableWithoutFeedback onPress={this.closeNotification}>
@@ -35,11 +88,30 @@ export default class PDFModal extends Component {
           animationType={'fade'}
           onRequestClose={this.closeNotification}
         >
+          <View style={styles.navBar}>
+            <TouchableOpacity
+              onPress={this.closeNotification}
+              style={styles.headerImage}
+            >
+              <Image
+                onPress={this.closeNotification}
+                source={require('../../assets/images/back.png')}
+                style={StyleSheet.flatten(styles.headerIcon)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this._downloadPdf} activeOpacity={0.2}>
+              <Image
+                onPress={this._downloadPdf}
+                source={require('../../assets/images/download_icon.png')}
+                style={StyleSheet.flatten(styles.downloadIcon)}
+              />
+            </TouchableOpacity>
+          </View>
           <View style={[styles.modalCont, style]}>
             <PDFReader
               style={{
                 width: Dimensions.get('window').width,
-                height: 600,
+                height: Dimensions.get('window').height,
                 backgroundColor: '#000',
               }}
               source={{
@@ -47,6 +119,18 @@ export default class PDFModal extends Component {
               }}
             />
           </View>
+          <ErrorAlert
+            title={'Error!'}
+            message={message}
+            handleCloseNotification={this.handleCloseNotification}
+            visible={showAlert}
+          />
+          <SuccessAlert
+            title={'Awesome!'}
+            message={message}
+            handleCloseNotification={this.handleCloseNotification}
+            visible={showAlert}
+          />
         </Modal>
       </TouchableWithoutFeedback>
     );
@@ -71,4 +155,42 @@ const styles = StyleSheet.create({
     // padding: 20,
     backgroundColor: 'rgba(0,0,0,.476)',
   },
+  headerImage: {
+    borderRadius: 30,
+    height: 40,
+    width: 40,
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerIcon: {
+    height: 18,
+    width: 18,
+    tintColor: colors.blacks,
+  },
+  downloadIcon: {
+    height: 25,
+    width: 25,
+    tintColor: colors.black,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginLeft: 50,
+  },
+  navBar: {
+    flexDirection: 'row',
+    paddingTop: Platform.OS === 'ios' ? 8 : StatusBar.currentHeight,
+    height: Platform.OS === 'ios' ? 50 : 64,
+    width: '100%',
+    alignItems: 'center',
+    backgroundColor: theme.colorAccent,
+    elevation: 1,
+    shadowOffset: { height: 1, width: 0 },
+    shadowColor: '#00000066',
+    shadowOpacity: 0.25,
+    shadowRadius: 2.26,
+    alignContent: 'space-between',
+    marginTop: Platform.OS === 'ios' ? 0 : -20,
+  },
 });
+
+export default PDFModal;
