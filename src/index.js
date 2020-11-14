b1ef21f098d8901ab0e66931f2e2199e5ee4b301
Akita Emmanuel <e.akita91@gmail.com>
2020-11-14 20:10:27 +0100

    Init sentry

--- src/index.js
+++ src/index.js
@@ -2,12 +2,27 @@
 
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, TextInput } from 'react-native';
+import * as Sentry from 'sentry-expo';
 import * as Font from 'expo-font';
 import Navigator from './routes';
 import colors from './assets/colors';
 TextInput.defaultProps.selectionColor = colors.text_color;
 import { Provider } from 'react-redux';
 import store from './redux/store';
+
+Sentry.init({
+  dsn:
+    'https://a19d70ab4d604ea19ac81a793b800b72@o476524.ingest.sentry.io/5516801',
+  enableInExpoDevelopment: true,
+  debug: true, // Sentry will try to print out useful debugging information if something goes wrong with sending an event. Set this to `false` in production.
+  setCommits: true,
+});
+
+// Access any @sentry/react-native exports via:
+// Sentry.Native.*
+
+// Access any @sentry/browser exports via:
+// Sentry.Browser.*
 
 const App = () => {
   const [fontsLoaded, setfontLoad] = useState(false);
