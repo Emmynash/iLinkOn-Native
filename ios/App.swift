defa441b941e763cdfd80eb19edffa114863ea79
Akita Emmanuel <e.akita91@gmail.com>
2020-12-06 04:25:02 +0100

    Auto gen files

--- ios/App.swift
+++
@@ -1,8 +0,0 @@
-//
-//  App.swift
-//  
-//
-//  Created by Akita on 05/12/2020.
-//
-
-import Foundation
--- ios/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by Akita on 05/12/2020.
-//
-
-import Foundation
